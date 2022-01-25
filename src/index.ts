510f4ec7601c9bc96c00373c9f23c3812e42d0cd
zak12368 <zakaria.boussoffara@polymtl.ca>
2022-01-25 09:53:52 -0500

    1 file modified

--- src/index.ts
+++ src/index.ts
@@ -18,16 +18,38 @@ import './style.css';
 
 let map: google.maps.Map;
 
+
+const stops: [google.maps.LatLngLiteral, string][] = [
+  [{ lat: 45.54660349710988, lng: -73.60924619686111 }, "Hôpital Jean-Talon"],
+  [{ lat: 34.8559195, lng: -111.7988186 }, "Airport Mesa"],
+  [{ lat: 34.832149, lng: -111.7695277 }, "Chapel of the Holy Cross"],
+  [{ lat: 34.823736, lng: -111.8001857 }, "Red Rock Crossing"],
+  [{ lat: 34.800326, lng: -111.7665047 }, "Bell Rock"],
+];
+
+
 function initMap(): void {
-  const cpa = { lat: 45.5381, lng: -73.61132 };
+  const cpa = { lat: 45.538155, lng: -73.611370 };
   map = new google.maps.Map(document.getElementById('map') as HTMLElement, {
     center: cpa,
     zoom: 15,
   });
 
+  const svgMarker = {
+    path: "M10.453 14.016l6.563-6.609-1.406-1.406-5.156 5.203-2.063-2.109-1.406 1.406zM12 2.016q2.906 0 4.945 2.039t2.039 4.945q0 1.453-0.727 3.328t-1.758 3.516-2.039 3.070-1.711 2.273l-0.75 0.797q-0.281-0.328-0.75-0.867t-1.688-2.156-2.133-3.141-1.664-3.445-0.75-3.375q0-2.906 2.039-4.945t4.945-2.039z",
+    fillColor: "red",
+    fillOpacity: 0.6,
+    strokeWeight: 0,
+    rotation: 0,
+    scale: 2,
+    anchor: new google.maps.Point(15, 30),
+  };
+
+
   new google.maps.Marker({
     position: cpa,
     map,
+    icon: svgMarker,
     title: "CPA MONTREAL",
   });
 
